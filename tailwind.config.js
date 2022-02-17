231d51fe7d751aab35889dee9bc8aca7627b7ef6
Ramz001 <rkenjaev1@gmail.com>
2022-02-17 19:01:35 +0500

    main

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'space-mono': ["Space Mono"]
+      },
+      colors:{
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": 'hsl(183, 100%, 15%)',
+        darkGrayishCyan:{
+          1: "hsl(186, 14%, 43%)",
+          2: "hsl(184, 14%, 56%)"
+        },
+        lightGrayishCyan:{
+          1: "hsl(185, 41%, 84%)",
+          2: "hsl(189, 41%, 97%)"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
