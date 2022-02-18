d9cb891199b8c50e0b8b6960b0cb646a02ac0c6f
Ramz001 <rkenjaev1@gmail.com>
2022-02-18 14:37:57 +0500

    mian

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,18 @@ module.exports = {
       colors:{
         "strong-cyan": "hsl(172, 67%, 45%)",
         "very-dark-cyan": 'hsl(183, 100%, 15%)',
+      
         darkGrayishCyan:{
           1: "hsl(186, 14%, 43%)",
           2: "hsl(184, 14%, 56%)"
         },
         lightGrayishCyan:{
           1: "hsl(185, 41%, 84%)",
-          2: "hsl(189, 41%, 97%)"
-        }
+          2: "hsl(189, 41%, 97%)" 
+          }
+        },
+      backgroundImage:{
+        "dollar-svg": "url(/images/icon-dollar.svg)"
       }
     },
   },
